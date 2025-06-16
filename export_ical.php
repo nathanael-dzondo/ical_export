2a7325fff14d5c4fae8e5c9b18231e8317b493c6
DZONDO Anthony Nathanaël <nathanaeldzondo@gmail.com>
2025-06-16 10:45:07 +0200

    test1

--- export_ical.php
+++ export_ical.php
@@ -1,29 +1,35 @@
 <?php
-// Connexion à la base de données
-$pdo = new PDO('mysql:host=localhost;dbname=sejour2025;charset=utf8', 'root', '');
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=sejour2025;charset=utf8', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
 
-// Lire les réservations confirmées
 $stmt = $pdo->query("SELECT arrive, depart FROM reservations");
 $reservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Début de l'export .ics
 header('Content-type: text/calendar; charset=utf-8');
 header('Content-Disposition: attachment; filename=calendar.ics');
 
 echo "BEGIN:VCALENDAR\r\n";
 echo "VERSION:2.0\r\n";
-echo "PRODID:-//nuratec.fr//ftp//stagedampierre//dzondoFR\r\n";
-foreach ($reservations as $res) {
-    $start = date('Ymd', strtotime($res['arrive']));
-    $end = date('Ymd', strtotime($res['depart'])); // jour de départ NON inclus en iCal
+echo "CALSCALE:GREGORIAN\r\n";
+echo "PRODID:-//nuratec.fr//ftp//stagedampierre//Fr\r\n";
 
-    echo "BEGIN:VEVENT\r\n";
-    echo "UID:" . uniqid() . "@tonsite.com\r\n";
-    echo "DTSTAMP:" . date('Ymd\THis\Z') . "\r\n";
-    echo "DTSTART;VALUE=DATE:$start\r\n";
-    echo "DTEND;VALUE=DATE:$end\r\n";
-    echo "SUMMARY:Réservé\r\n";
-    echo "END:VEVENT\r\n";
+foreach ($reservations as $res) {
+    if (!empty($res['arrive']) && !empty($res['depart'])) {
+        $start = date('Ymd', strtotime($res['arrive']));
+        $end = date('Ymd', strtotime($res['depart']));
+
+        echo "BEGIN:VEVENT\r\n";
+        echo "UID:" . uniqid() . "123nuratec@gmail.com\r\n";
+        echo "DTSTAMP:" . date('Ymd\THis\Z') . "\r\n";
+        echo "DTSTART;VALUE=DATE:$start\r\n";
+        echo "DTEND;VALUE=DATE:$end\r\n";
+        echo "SUMMARY:Réservé\r\n";
+        echo "END:VEVENT\r\n";
+    }
 }
 
 echo "END:VCALENDAR\r\n";
